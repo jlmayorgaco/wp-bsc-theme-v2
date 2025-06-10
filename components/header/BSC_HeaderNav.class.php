2ab93784b04940384ba5396350608dc2ebd09b8a
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-06-10 14:59:22 -0500

    Create BSC_HeaderNav.class.php

---
+++ components/header/BSC_HeaderNav.class.php
@@ -0,0 +1,55 @@
+<?php
+
+class BSC_HeaderNav {
+	private string $id = '';
+	private string $class = '';
+	private array $menus = [];
+
+	public function setId(string $id): void {
+		$this->id = $id;
+	}
+
+	public function setClass(string $class): void {
+		$this->class = $class;
+	}
+
+	public function setMenus(array $menus): void {
+		$this->menus = $menus;
+	}
+
+	public function getMenus(): array {
+		return $this->menus;
+	}
+
+	public function addMenu(BSC_MenuNav $menu): void {
+		$this->menus[] = $menu;
+	}
+
+	public function renderNavButtons(): string {
+		ob_start();
+		echo '<nav id="' . esc_attr($this->id) . '" class="' . esc_attr($this->class) . '">';
+
+		foreach ($this->menus as $menu) {
+			if ($menu instanceof BSC_MenuNav) {
+				echo $menu->renderButton();
+			}
+		}
+
+		echo '</nav>';
+		return ob_get_clean();
+	}
+
+    	public function renderNavContent(): string {
+		ob_start();
+		echo '<nav id="' . esc_attr($this->id) . '" class="' . esc_attr($this->class) . '">';
+
+		foreach ($this->menus as $menu) {
+			if ($menu instanceof BSC_MenuNav) {
+				echo $menu->renderContent();
+			}
+		}
+
+		echo '</nav>';
+		return ob_get_clean();
+	}
+}
