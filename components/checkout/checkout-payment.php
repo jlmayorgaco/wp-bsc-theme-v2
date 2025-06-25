bb12bb79dbec1c7358b9b56264b2f03b74247226
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-06-25 10:34:34 -0500

    Create checkout-payment.php

---
+++ components/checkout/checkout-payment.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+<div class="bsc bsc-checkout__payments">
+    <div class="checkout__payments-container">
+    <div id="order_review" class="bsc-checkout__review woocommerce-checkout-review-order">
+        <?php woocommerce_checkout_payment(); ?>
+    </div>
+    </div>
+</div>
