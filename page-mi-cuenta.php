1d1f5c185aec8bccd48060dcbf4e983ceb506eef
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-07-22 16:11:18 -0500

    Create page-mi-cuenta.php

---
+++ page-mi-cuenta.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Página Mi Cuenta Personalizada
+ */
+
+get_header();
+?>
+
+<main class="bsc bsc__page bsc__page--account">
+  <div class="bsc__container bsc__account-container">
+
+    <h1 class="bsc__title bsc__title--centered">Mi cuenta</h1>
+
+    <div class="bsc__account-wrapper">
+      <?php
+        // Renderiza contenido de WooCommerce My Account (incluye dashboard, pedidos, detalles, etc.)
+        echo do_shortcode('[woocommerce_my_account]');
+      ?>
+    </div>
+
+  </div>
+</main>
+
+<?php get_footer(); ?>
