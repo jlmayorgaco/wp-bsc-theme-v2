e9598b302c3fdcfbc2068221f059c9e5743991c5
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-07-22 16:10:27 -0500

    Create checkout-actions.php

---
+++ inc/ajax/checkout-actions.php
@@ -0,0 +1,29 @@
+<?php
+
+add_action('wp_ajax_bsc_reload_city_fields', 'bsc_reload_city_fields');
+add_action('wp_ajax_nopriv_bsc_reload_city_fields', 'bsc_reload_city_fields');
+
+function bsc_reload_city_fields() {
+  $checkout = WC()->checkout();
+  $fields = $checkout->get_checkout_fields();
+  $state = sanitize_text_field($_POST['billing_state'] ?? '');
+
+  // (Optional) Update value based on state if needed
+  $_POST['billing_state'] = $state;
+
+  ob_start();
+  ?>
+    <?php
+    woocommerce_form_field('billing_city', $fields['billing']['billing_city'], '');
+    ?>
+  <?php
+  $html = ob_get_clean();
+
+  wp_send_json_success(['html' => $html]);
+}
+
+
+
+
+
+?>
