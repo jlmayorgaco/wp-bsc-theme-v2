2b6fa435f6172e86468733c3d72dd6d389b99c92
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-08-13 11:38:51 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -24,6 +24,7 @@ require get_template_directory() . '/inc/custom-header.php';
 require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/inc/functions_bsc.php';
 
 // Load Jetpack compatibility file.
 if ( defined( 'JETPACK__VERSION' ) ) {
