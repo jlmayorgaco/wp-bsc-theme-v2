f88b567d6efb7e8f356e4af9e1fb74cfed3b0ae3
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-08-20 08:00:10 -0500

    Update page-mi-cuenta.php

--- page-mi-cuenta.php
+++ page-mi-cuenta.php
@@ -9,8 +9,6 @@ get_header();
 <main class="bsc bsc__page bsc__page--account">
   <div class="bsc__container bsc__account-container">
 
-    <h1 class="bsc__title bsc__title--centered">Mi cuenta</h1>
-
     <div class="bsc__account-wrapper">
       <?php
         // Renderiza contenido de WooCommerce My Account (incluye dashboard, pedidos, detalles, etc.)
