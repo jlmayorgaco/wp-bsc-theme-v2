8f73c534f85c6b4d891787a54cd39a5cc9d0033d
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-08-20 08:00:12 -0500

    Create page-bubble-points.php

---
+++ page-bubble-points.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Bubble Points
+ */
+
+get_header(); ?>
+
+<main id="primary" class="site-main bsc bsc__bubble-points">
+
+<nav class="woocommerce-MyAccount-navigation" aria-label="<?php esc_html_e( 'Account pages', 'woocommerce' ); ?>">
+ <?php
+
+	require_once get_template_directory() . '/components/my-account/my-account-header.php';
+	$headerMyAccount = new BSC_MY_ACCOUNT_HEADER();
+	$headerMyAccount->render();
+
+ ?>
+</nav>
+
+  <div class="container">
+      <?php require_once get_template_directory() . '/plugins/bubble-points/views/profile-bubble-points.php'; ?>
+  </div>
+  
+</main>
+
+<?php get_footer(); ?>
