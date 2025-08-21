66565b76e227c88d4250812e56f68fabd17560ef
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-08-21 10:48:33 -0500

    Create coupons.php

---
+++ plugins/bubble-points/config/coupons.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Coupon configuration for Bubble Points system
+ */
+
+return [
+    [
+        'color'  => 'yellow',
+        'price'  => 75000,
+        'points' => 1000,
+        'image'  => 'coupon_c1__figure',
+        'text'   => 'en skin care, hair care, maquillaje, inner beauty y dispositivos coreanos en BSC',
+        'tag'    => 'BSC_COUPON_1000'
+    ],
+    [
+        'color'  => 'blue',
+        'price'  => 110000,
+        'points' => 1470,
+        'image'  => 'coupon_c1__figure',
+        'text'   => 'en skin care, hair care, maquillaje, inner beauty y dispositivos coreanos en BSC',
+        'tag'    => 'BSC_COUPON_1470'
+    ],
+    [
+        'color'  => 'pink',
+        'price'  => 160000,
+        'points' => 2130,
+        'image'  => 'coupon_c1__figure',
+        'text'   => 'en skin care, hair care, maquillaje, inner beauty y dispositivos coreanos en BSC',
+        'tag'    => 'BSC_COUPON_2130'
+    ],
+    [
+        'color'  => 'lilac',
+        'price'  => 220000,
+        'points' => 2930,
+        'image'  => 'coupon_c1__figure',
+        'text'   => 'en skin care, hair care, maquillaje, inner beauty y dispositivos coreanos en BSC',
+        'tag'    => 'BSC_COUPON_2930'
+    ],
+    [
+        'color'  => 'orange',
+        'price'  => 290000,
+        'points' => 3870,
+        'image'  => 'coupon_c1__figure',
+        'text'   => 'en skin care, hair care, maquillaje, inner beauty y dispositivos coreanos en BSC',
+        'tag'    => 'BSC_COUPON_3870'
+    ],
+    [
+        'color'  => 'green',
+        'price'  => 360000,
+        'points' => 4800,
+        'image'  => 'coupon_c1__figure',
+        'text'   => 'en skin care, hair care, maquillaje, inner beauty y dispositivos coreanos en BSC',
+        'tag'    => 'BSC_COUPON_4800'
+    ],
+    [
+        'color'  => 'gold',
+        'price'  => 430000,
+        'points' => 5730,
+        'image'  => 'coupon_c1__figure',
+        'text'   => 'en skin care, hair care, maquillaje, inner beauty y dispositivos coreanos en BSC',
+        'tag'    => 'BSC_COUPON_5730'
+    ],
+    [
+        'color'  => 'aqua',
+        'price'  => 500000,
+        'points' => 6660,
+        'image'  => 'coupon_c1__figure',
+        'text'   => 'en skin care, hair care, maquillaje, inner beauty y dispositivos coreanos en BSC',
+        'tag'    => 'BSC_COUPON_6660'
+    ],
+    [
+        'color'  => 'rose',
+        'price'  => 680000,
+        'points' => 9060,
+        'image'  => 'coupon_c1__figure',
+        'text'   => 'en skin care, hair care, maquillaje, inner beauty y dispositivos coreanos en BSC',
+        'tag'    => 'BSC_COUPON_9060'
+    ],
+];
