3f0ed3b804cfdf523f20c15e04b18dc534d4c0e4
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-09-03 15:45:35 -0500

    Update hooks-front.php

--- plugins/bubble-points/hooks/hooks-front.php
+++ plugins/bubble-points/hooks/hooks-front.php
@@ -1,4 +1,21 @@
 <?php
+defined('ABSPATH') || exit;
 
+add_action('wp_enqueue_scripts', 'bsc_bp_enqueue_scripts');
 
-?>
+function bsc_bp_enqueue_scripts() {
+
+        wp_enqueue_script(
+            'bsc-bubble-points-modal',
+            get_stylesheet_directory_uri() . '/plugins/bubble-points/scripts/bubble-points-modal.js',
+            ['jquery'],
+            '1.0.0',
+            true
+        );
+
+        wp_localize_script('bsc-bubble-points-modal', 'bsc_points', [
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce'    => wp_create_nonce('bsc_redeem_points')
+        ]);
+
+}
