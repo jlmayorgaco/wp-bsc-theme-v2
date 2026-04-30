a88b3e35466a6f2ad66864f3ccc4450df7123749
Ing. Jorge Luis Mayorga Taborda <167396627+jlmayorgaco@users.noreply.github.com>
2026-04-30 16:56:21 -0500

    fix(orders): [BSC-149] simplify my-account list progress bar

--- components/orders/orders-table.php
+++ components/orders/orders-table.php
@@ -162,6 +162,7 @@ class BSC_Orders_Table {
         $progress = new BSC_Order_Progress_Bar(
             bsc_map_order_status_to_bar($order->get_status())
         );
+        $progress->setDisplayMode(BSC_Order_Progress_Bar::DISPLAY_COMPACT);
         $progress->render();
     }
 }
